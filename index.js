810dab22508de279610b190890d39dc3c48d8174
Finn Thye <piqueen@gmail.com>
2019-04-12 00:57:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
- object[key] = value;
- return object
+ object.key = value;
+ return object;
 }
 
 function deleteFromObjectByKey(object, key) {
