d30064782e7f45e562ec811198615a319b9f3a4c
Finn Thye <piqueen@gmail.com>
2019-04-12 01:05:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign()
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
