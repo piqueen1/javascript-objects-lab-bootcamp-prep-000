52abe7d3565c85be706db0ec9ddd19a7460d59bc
Finn Thye <piqueen@gmail.com>
2019-04-12 00:56:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-
+ object.key = value;
+ return object
 }
 
 function deleteFromObjectByKey(object, key) {
